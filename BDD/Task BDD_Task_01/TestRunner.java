1bbeb12646bb1321e8f67db0b90993cfe76feab7
jaiganesh9319 <54517086+jaiganesh9319@users.noreply.github.com>
2024-09-09 16:26:51 +0530

    Update TestRunner.java

--- BDD/Task BDD_Task_01/TestRunner.java
+++ BDD/Task BDD_Task_01/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = "com.example",
+        glue = "jai.jai",
         plugin = {"pretty"}
 )
 public class TestRunner {
