d23ef49ac945447a484a865faf0f2e8293076e7a
jaiganesh9319 <54517086+jaiganesh9319@users.noreply.github.com>
2024-09-18 17:15:08 +0530

    Add files via upload

---
+++ Build Automation using Maven/Maven_report/mvnreport/src/test/java/mvnreport/mvnreport/AppTest.java
@@ -0,0 +1,11 @@
+package mvnreport.mvnreport;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AppTest {
+    @Test
+    public void sampleTest() {
+        Assert.assertTrue(true, "Sample test passed!");
+    }
+}
